767f7b7f5396f36648f804442f781f72344be984
Rikuya37-maker <83212761+Rikuya37-maker@users.noreply.github.com>
2021-04-26 19:18:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,19 @@
-from discord.ext import commands
-import os
-import traceback
+# インストールした discord.py を読み込む
+import discord
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'ODMxODA1Mjg2NDMxNDU3MzIw.YHalCg.pF3tXPHNb7NWp8rMOcQqtgsXilc'
+
+# 接続に必要なオブジェクトを生成
 client = discord.Client()
 
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
@@ -15,4 +23,5 @@ async def on_message(message):
     if message.content == '/neko':
         await message.channel.send('にゃーん')
 
-client.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
